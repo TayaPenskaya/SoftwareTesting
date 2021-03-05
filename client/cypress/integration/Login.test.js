4ad463459752746f9fce0c3bf86d24a501b66d26
tpenskaya <tayaserf@gmail.com>
2021-03-05 16:16:37 +0300

    Added cypress tests

---
+++ client/cypress/integration/Login.test.js
@@ -0,0 +1,25 @@
+describe('The Login Page', () => {
+    beforeEach(() => {
+        // reset and seed the database prior to every test
+        cy.exec('npm start')
+    });
+
+    it('successfully loads', () => {
+        cy.visit('/')
+    });
+
+    it('successfully login', () => {
+        cy.visit('/');
+        cy.get('#login-id')
+            .invoke('attr', 'value', 'taya')
+            .trigger('change');
+        cy.get('#password-id')
+            .invoke('attr', 'value', 'taya')
+            .trigger('change');
+        cy.get('#submit-id').click();
+        cy.location().should((loc) => {
+            expect(loc.pathname).to.eq('/tables')
+        });
+    });
+
+});
